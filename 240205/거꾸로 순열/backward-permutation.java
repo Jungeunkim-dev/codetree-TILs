b18947adc642e169e2cdefca9c4d8043a30b77c3
김정은 <67693142+Jungeunkim-dev@users.noreply.github.com>
2024-02-05 21:38:02 +0900

    Title: 거꾸로 순열, Time: 1328ms, Memory: 38MB, Status: Passed - CodeTree

---
+++ 240205/거꾸로 순열/backward-permutation.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Main {
+
+    public static int n;
+    public static List<Integer> arr = new ArrayList<>();
+    public static boolean[] visited;
+
+    public static void printArr() {
+
+        for(int i=0; i<arr.size(); i++) {
+            System.out.print(arr.get(i)+" ");
+        }
+
+        System.out.println();
+    }
+
+    public static void choose(int current) {
+        
+        if (current == n) {
+            printArr();
+            return;
+        }
+
+        for (int i=n; i>=1; i--) {
+
+            if (visited[i]) {
+                continue;
+            } 
+
+            visited[i] = true;
+            arr.add(i);
+
+            choose(current+1);
+
+            visited[i] = false;
+            arr.remove(arr.size()-1);
+
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        visited = new boolean[n+1];
+        Arrays.fill(visited, false);
+
+        choose(0);
+    }
+}
