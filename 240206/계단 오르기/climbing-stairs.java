e3edf711523125d3674a1fab363b854f60225586
김정은 <67693142+Jungeunkim-dev@users.noreply.github.com>
2024-02-06 23:23:42 +0900

    Title: 계단 오르기, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240206/계단 오르기/climbing-stairs.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] dp = new int[n+1];
+
+        if (n <= 3){
+            System.out.println(1);
+        } else {
+
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 1;
+
+        for(int i=4; i<=n; i++) {
+            dp[i] = dp[i-2] + dp[i-3];
+        }
+
+        System.out.println(dp[n]);
+        }
+    }
+}
