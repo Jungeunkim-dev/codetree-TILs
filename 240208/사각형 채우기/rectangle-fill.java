e56d67c3ddbf25fd7d96fea1c0ec5af3351263eb
김정은 <67693142+Jungeunkim-dev@users.noreply.github.com>
2024-02-08 16:42:49 +0900

    Title: 사각형 채우기, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240208/사각형 채우기/rectangle-fill.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int dp[] = new int[n+1];
+
+        if (n <= 2){
+            System.out.println(n);
+        } else {
+
+        
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3; i<=n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2])%10007;
+        }
+
+
+        System.out.println(dp[n]);
+        
+        
+        }
+
+    }
+}
