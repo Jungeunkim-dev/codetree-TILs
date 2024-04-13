2f336fa2c8b2a60232ab81e192d3b96bc45798ce
김정은 <67693142+Jungeunkim-dev@users.noreply.github.com>
2024-04-14 00:32:09 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 934ms, Memory: 39MB, Status: Passed - CodeTree

--- 240414/연속 부분 합의 최댓값 구하기/max-of-partial-sum.java
+++ 240414/연속 부분 합의 최댓값 구하기/max-of-partial-sum.java
@@ -7,24 +7,28 @@ public class Main {
 
         int n = sc.nextInt();
 
-        int[] data = new int[n+1];
+        int[] data = new int[n];
 
-        for(int i=1; i<=n; i++) {
+        for(int i=0; i<n; i++) {
             data[i] = sc.nextInt();
         }
 
-        int[] dp = new int[n+1];
+        int[] dp = new int[n];
 
-        for(int i=1; i<=n; i++) {
+        for(int i=0; i<n; i++) {
             dp[i] = data[i];
         }
 
-        for(int i=1; i<=n; i++) {
-            dp[i] = Math.max(dp[i], dp[i-1] + data[i]);
+        
+
+        for(int i=1; i<n; i++) {
+            dp[i] = Math.max(data[i], dp[i-1] + data[i]);
         }
 
         Arrays.sort(dp);
 
-        System.out.println(dp[n]);
+        // System.out.println(Arrays.toString(dp));
+
+        System.out.println(dp[n-1]);
     }
 }
