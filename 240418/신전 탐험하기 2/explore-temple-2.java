a7da113664eceb45c342d755d20a034e35af010f
김정은 <67693142+Jungeunkim-dev@users.noreply.github.com>
2024-04-18 12:33:16 +0900

    Title: 신전 탐험하기 2, Time: 139ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240418/신전 탐험하기 2/explore-temple-2.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[][] room = new int[n+1][3];
+
+        for(int i=1; i<=n; i++) {
+            room[i][0] = sc.nextInt();
+            room[i][1] = sc.nextInt();
+            room[i][2] = sc.nextInt();
+        }
+
+        int[][] dp = new int[n+1][3];
+        int ans = 0; 
+
+
+        for(int start=0; start<3; start++){
+
+            for(int i=0;i<3; i++){
+                if(i==start) continue;
+                dp[1][start] = room[1][start];
+            }
+
+            for(int i=2; i<=n; i++) {
+                for(int j=0; j<3; j++) {
+                    for(int k=0; k<3; k++) {
+                        if (j==k) continue;
+
+                        dp[i][j] = Math.max(dp[i][j], dp[i-1][k] + room[i][j]);
+                    }
+                }
+            }
+
+
+            for(int i=0; i<3; i++) {
+                ans = Math.max(dp[n][i], ans);
+            }
+        }
+
+
+        System.out.println(ans);
+    }
+}
