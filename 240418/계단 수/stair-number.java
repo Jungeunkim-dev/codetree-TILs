b7cf8be4a515383f5f0d1e7bcb9639dda13f3099
김정은 <67693142+Jungeunkim-dev@users.noreply.github.com>
2024-04-18 12:08:26 +0900

    Title: 계단 수, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 240418/계단 수/stair-number.java
+++ 240418/계단 수/stair-number.java
@@ -1,13 +1,16 @@
 import java.util.*;
 
 public class Main {
+
+    static final int MOD = 1000000007;
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
 
-        long[][] dp = new long[n+1][11];
+        long[][] dp = new long[n+1][15];
 
         for(int i=1; i<=9; i++) {
             dp[1][i] = 1;
@@ -33,11 +36,11 @@ public class Main {
             for(int j=0; j<=9; j++) {
 
                 if (j > 0) {
-                    dp[i+1][j-1] = (dp[i+1][j-1] + dp[i][j]) % 1000000007;
+                    dp[i+1][j-1] = (dp[i+1][j-1] + dp[i][j]) % MOD;
                 }
 
                 if (j < 9) {
-                    dp[i+1][j+1] = (dp[i+1][j+1] + dp[i][j]) % 1000000007;
+                    dp[i+1][j+1] = (dp[i+1][j+1] + dp[i][j]) % MOD;
                 }
             }
         }
@@ -45,7 +48,7 @@ public class Main {
         long ans = 0;
 
         for(int i=0; i<=9; i++) {
-            ans += (dp[n][i]);
+            ans = (ans + dp[n][i]) % 1000000007;
         }
 
         System.out.println(ans);
