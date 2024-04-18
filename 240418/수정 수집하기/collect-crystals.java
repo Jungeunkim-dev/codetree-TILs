5ae90ed4851b22fc1fe9dbb4fc57a0d6012c356f
김정은 <67693142+Jungeunkim-dev@users.noreply.github.com>
2024-04-18 13:27:23 +0900

    Title: 수정 수집하기, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240418/수정 수집하기/collect-crystals.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in); 
+
+        int n = sc.nextInt();
+        int kMax = sc.nextInt();
+
+        String jem = sc.next();
+
+        int[][][] dp = new int[n+1][2][kMax+1];
+        
+        char[] pos = {'L','R'};
+
+        // 0:L, 1:R
+
+        if(jem.charAt(0)=='L') {
+            dp[1][0][0] = 1;
+        }
+
+
+        if(jem.charAt(0)=='R') {
+            dp[1][1][0] = 1;
+        }
+
+        for(int i=2; i<=n; i++) {
+
+            for(int j=0; j<2; j++) {
+
+                for(int k=0; k <= kMax; k++) {
+
+                    if (k > i-1) continue;
+                    
+                    if (k==0) {
+                        dp[i][j][0] = dp[i-1][j][0];
+                    
+                    } else {
+
+                        int value = jem.charAt(i-1)=='L' ? 0 : 1;
+                        int count = 0;
+
+                        if (j == value) {
+                            count = 1;
+                        }
+
+                        dp[i][j][k] = Math.max(dp[i-1][j][k], dp[i-1][ (j) % 2][k-1]) + count;
+                    }
+                }
+
+            }
+        }
+
+        int ans = 0;
+
+        for(int i=0; i<2; i++) {
+            for(int k=0; k<=kMax; k++) {
+                ans = Math.max(ans, dp[n][i][k]);
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
